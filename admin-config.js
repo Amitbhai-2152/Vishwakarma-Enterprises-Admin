3d1f9210c4c2a7dc606b33f3bc1c879ebb7a08c6
Amit Raj <navodaya2152@gmail.com>
2026-09-14 13:33:45 +0530

    Configure GitHub App client authentication

--- admin-config.js
+++ admin-config.js
@@ -2,5 +2,6 @@ window.ADMIN_CONFIG={
   dataUrl:'https://raw.githubusercontent.com/Amitbhai-2152/Vishwakarma-Enterprises-Admin/main/data/products.json',
   repoOwner:'Amitbhai-2152',
   repoName:'Vishwakarma-Enterprises-Admin',
-  dataPath:'data/products.json'
+  dataPath:'data/products.json',
+  githubClientId:'Iv23licQXQzKHN3uSRqS'
 };
